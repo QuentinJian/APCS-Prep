186b886032e8ef5cbf2fd312c176eaa05b89b5ad
QuentinJian <52318767+QuentinJian@users.noreply.github.com>
2022-10-17 22:34:17 +0800

    test pointer utility

--- Simlulation/SmallGrouping.cpp
+++ Simlulation/SmallGrouping.cpp
@@ -1,13 +1,28 @@
-#include <iostream>
-#include <vector>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main() {
     int nums_size;
-    vector<int>
+    int travel = 0;
+    int ans = 0;
     cin >> nums_size;
+    vector<int> nums(nums_size);
+    map<int, bool> searched;
     for (int i=0;i<nums_size;i++) {
-
+        cin >> nums[i];
     }
+    for (int i=0;i<nums.size();i++) {
+        if (searched[i] == true)
+            break; 
+        int cur = nums[i];
+        travel = nums[i];
+        do {
+            searched[nums[travel]] = true;
+            travel = nums[travel];
+        }while(travel != cur);
+        ans++;
+    }
+    cout << ans << endl;
+    return 0;
 }
